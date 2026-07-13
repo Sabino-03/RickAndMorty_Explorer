ddffdbfda371cc23d9520aabe58f5ab895d22ac7
Sabino-03 <elio.sab2003@gmail.com>
2026-07-13 21:49:00 +0200

    define login-component (uses a formGroup for userName and passWord; submit with a button); implements an AuthService [day 1]

---
+++ src/app/components/login-component/login.component.ts
@@ -0,0 +1,62 @@
+import { Component, inject, OnDestroy, OnInit } from "@angular/core";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
+import { BehaviorSubject, debounceTime, Subject, takeUntil } from "rxjs";
+import { AuthService } from "../../services/auth.service";
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.html',
+    styles: [],
+    imports: [ ReactiveFormsModule ]
+})
+
+export class LoginComponent implements OnInit, OnDestroy {
+
+    private router = inject(Router);
+    private authService = inject(AuthService);
+
+    public loginForm = new FormGroup<any>({
+        userName : new FormControl(),
+        passWord : new FormControl()
+    })
+
+    private destroy$ : Subject<void> = new Subject<void>();
+    private username$ : BehaviorSubject<string> = new BehaviorSubject<string>('');
+    private password$ : BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+    ngOnInit() : void {
+        this.loginForm.get('userName')?.valueChanges
+        .pipe(
+            debounceTime(300),
+            takeUntil(this.destroy$)
+        )
+        .subscribe({
+            next : ((value : string) => { this.username$.next(value) }),
+            error : ((err) => {}),
+            complete : (() => {})
+        })
+
+        this.loginForm.get('passWord')?.valueChanges
+        .pipe(
+            debounceTime(300),
+            takeUntil(this.destroy$)
+        )
+        .subscribe({
+            next : ((value : string) => { this.password$.next(value) }),
+            error : ((err) => {}),
+            complete : (() => {})
+        })
+    }
+
+    ngOnDestroy() : void {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
+
+    onSubmit() : void {
+        const authentication = this.authService.isAuthenticated(this.username$.getValue(), this.password$.getValue());
+        authentication ? this.router.navigate(['explore']) : false
+    }
+
+}
