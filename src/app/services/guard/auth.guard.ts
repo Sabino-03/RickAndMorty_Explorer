04d513c92decc9537431dffd66a2433003e82676
Sabino-03 <elio.sab2003@gmail.com>
2026-07-14 10:39:00 +0200

    define AuthGuard (simple redirect to login page) [day 4]

---
+++ src/app/services/guard/auth.guard.ts
@@ -0,0 +1,12 @@
+import { inject } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from "@angular/router";
+import { AuthService } from "../auth.service";
+
+export const AuthGuard : CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+
+    const router = inject(Router);
+    const authService = inject(AuthService);
+
+    return authService.isLoggedIn$.getValue() ? true : router.createUrlTree(['']);
+
+}
