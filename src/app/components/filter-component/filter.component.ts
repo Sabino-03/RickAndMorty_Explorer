5ccbf6ddfae8012231ed214e5cd7e4f7e184279c
Sabino-03 <elio.sab2003@gmail.com>
2026-07-20 12:58:00 +0200

    define FilterComponent (shows filters) [day 10]

---
+++ src/app/components/filter-component/filter.component.ts
@@ -0,0 +1,49 @@
+import { NgIf } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { ButtonComponent } from "../button-component/button.component";
+import { FilterService } from "../../services/filter.service";
+
+@Component({
+    selector: 'app-filter',
+    template :
+    `
+    <div class="relative flex flex-col items-center justify-center" >
+            <app-button [label]="'Status Filters'"
+                        [size]="'medium'"
+                        [type]="'button'"
+                        [variant]="'operations'"
+                        (clicked)="this.filterService.showFilters()" ></app-button>
+
+            <div *ngIf="this.filterService.isActivated" class="absolute top-full mt-1 z-50 flex flex-col items-center gap-1" >
+                <app-button [label]="'Alive'"
+                            [size]="'medium'"
+                            [type]="'button'"
+                            [variant]="'dropDown'"
+                            (clicked)="this.filterService.onClickSetAlive()" ></app-button>
+
+                <app-button [label]="'Dead'"
+                            [size]="'medium'"
+                            [type]="'button'"
+                            [variant]="'dropDown'"
+                            (clicked)="this.filterService.onClickSetDead()" ></app-button>
+
+                <app-button [label]="'unknown'"
+                            [size]="'medium'"
+                            [type]="'button'"
+                            [variant]="'dropDown'"
+                            (clicked)="this.filterService.onClickSetunknown()" ></app-button>
+            </div>
+    </div>
+    `,
+    styles: [],
+    imports: [
+        NgIf,
+        ButtonComponent
+    ]
+})
+
+export class FilterComponent {
+
+    public filterService = inject(FilterService);
+
+}
