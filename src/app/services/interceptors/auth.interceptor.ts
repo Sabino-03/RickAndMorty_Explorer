1499225d26105cb43fddd08b43a966c2149bf041
Sabino-03 <elio.sab2003@gmail.com>
2026-07-20 13:10:55 +0200

    define AuthInterceptor (setHeaders for Auth token) [day 10]

---
+++ src/app/services/interceptors/auth.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { AuthService } from "../auth.service";
+
+export const AuthInterceptor : HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
+
+    const authService = inject(AuthService);
+    const token = authService.getToken();
+
+    if(!token)
+        return next(req);
+
+    const cloneReq = req.clone({ setHeaders : { Authorization : `Bearer ${token}` } });
+
+    return next(cloneReq);
+
+}
