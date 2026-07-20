c9da73adba2f257fe27f00c0dbb4ca1d4ab8433c
Sabino-03 <elio.sab2003@gmail.com>
2026-07-20 21:49:00 +0200

    manage favourite_character feature for localStorage [day 10]

--- src/app/services/favourite.service.ts
+++ src/app/services/favourite.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from "@angular/core";
-import { CharacterInfo } from "../models/character";
+import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { SearchService } from "./search.service";
+import { CharacterInfo, CharacterResults } from "../models/character";
 
 @Injectable({providedIn: 'root'})
 export class FavouriteService {
 
+    private searchService = inject(SearchService);
+    private counter : number = Number(localStorage.getItem("Number Of Favourites"));
+    private favouriteLocalId : number = 0;
+    private name : string = "";
+    private status : string = "";
+
+    getNumberOfFavourites() : number { return this.counter; }
+
+    getFavouriteCharacter() : Observable<CharacterResults> { return this.searchService.toSearchNameStatus(this.name, this.status); }
+
     setFavouriteCharacter(character : CharacterInfo) : void {
-        localStorage.setItem(`Favourite: ${character.Nome.toString()}`, `Specie: ${character.Specie.toString()}, Stato: ${character.Stato.toString()}`);
+        this.favouriteLocalId = ++this.counter;
+        localStorage.setItem("Number Of Favourites", `${this.counter}`);
+        localStorage.setItem(`Favourite Character Name : ${this.favouriteLocalId}`, `${character.Nome}`);
+        localStorage.setItem(`Favourite Character Status : ${this.favouriteLocalId}`, `${character.Stato}`);
+
+        this.name = character.Nome ? character.Nome : "" ;
+        this.status = character.Stato ? character.Stato : "" ;
     }
 
 }
